aa96abf8a5b916cd5e6205bd36a51b761a334c81
Richard <ric.abitbol@gmail.com>
2020-03-14 03:19:48 +0100

    Navbar

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,19 +6,22 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('roles')
             ->add('password')
             ->add('account_name')
             ->add('img_path')
-            ->add('available_time')
-            ->add('score')
+            ->add('available_time', IntegerType::class, [
+                'label' => 'Available Time per Week'])
+            ->add('score', IntegerType::class, [
+                'label' => 'Score'])
             ->add('created_at')
             ->add('updated_at')
             ->add('program_bookmarks')
