8bb03e9f7d8dc157f6de5123669e980fe037a36e
Richard <ric.abitbol@gmail.com>
2020-03-14 03:43:01 +0100

    Fix UserType Issue

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,10 +3,16 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\Program;
+use App\Entity\Exercise;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class UserType extends AbstractType
 {
@@ -14,19 +20,37 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class)
-            ->add('roles')
-            ->add('password')
-            ->add('account_name')
-            ->add('img_path')
+            ->add('roles', CollectionType::class)
+           // ->add('password')
+            ->add('account_name', TextType::class)
+            ->add('img_path', TextType::class)
             ->add('available_time', IntegerType::class, [
                 'label' => 'Available Time per Week'])
             ->add('score', IntegerType::class, [
-                'label' => 'Score'])
-            ->add('created_at')
-            ->add('updated_at')
-            ->add('program_bookmarks')
-            ->add('exercise_bookmarks')
-            ->add('followed_programs')
+                'label' => 'score'])
+           // ->add('created_at')
+          //  ->add('updated_at')   @TODO Add to Controller before flush
+           //->add('program_bookmarks', ColectionType::class, [
+           //      'class' => Program::class,
+           //    'choice_label' => 'id',
+           //    'expanded' => true,
+           //    'multiple' => true,
+           //]
+           //)
+           //->add('exercise_bookmarks', EntityType::class, [
+           //    'class' => Exercise::class,
+           //    'choice_label' => 'id',
+           //    'expanded' => true,
+           //    'multiple' => true,
+           //]
+           //)
+           //->add('followed_programs', EntityType::class, [
+           //    'class' => Program::class,
+           //    'choice_label' => 'id',
+           //    'expanded' => true,
+           //    'multiple' => true,
+           //]
+           //)
         ;
     }
 
